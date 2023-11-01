0b5e8dc417a55c0b38b83b9018482c3b213788c4
JackNarvaez <janarvaez@unal.edu.co>
2023-11-01 10:04:03 -0500

    Improve colormap in cluster's image

--- pic/plotGrid.py
+++ pic/plotGrid.py
@@ -1,13 +1,28 @@
 import matplotlib.pyplot as plt
+from  matplotlib.colors import LinearSegmentedColormap
+from random import random
 import numpy as np
 
-# Read Data
-#Data = np.loadtxt("Grid.txt");
+''' ---------- Plot Grid ---------- '''
+Data = np.loadtxt("Grid.txt");
+fig, ax = plt.subplots(figsize=(8,8))
+ax.matshow(Data, cmap='Greys',  interpolation='nearest', origin='lower')
+ax.set_xlabel("$x$")
+ax.set_ylabel("$y$")
+plt.savefig("Grid.png", transparent=True, dpi=300)
+plt.show()
+
+
+''' ----- Define Random Color Map -----'''
+colors = [(1,1,1)] + [(random(),random(),random()) for i in range(255)]
+new_map = LinearSegmentedColormap.from_list('new_map', colors, N=256)
+
+
+''' ---------- Plot Grid ---------- '''
 DataC = np.loadtxt("Cluster.txt");
-
-# Plot Data
-#plt.matshow(Data)
-#plt.show()
-
-plt.matshow(DataC)
+fig, ax = plt.subplots(figsize=(8,8))
+ax.matshow(DataC, cmap=new_map,  interpolation='nearest', origin='lower')
+ax.set_xlabel("$x$")
+ax.set_ylabel("$y$")
+plt.savefig("Cluster.png", transparent=True, dpi=300)
 plt.show()
