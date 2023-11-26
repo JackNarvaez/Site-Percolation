8a0bcd6301fc16e13569cef6527654d177a62c1d
JackNarvaez <janarvaez@unal.edu.co>
2023-11-25 20:44:00 -0500

    Include Zoom image and example images for L=15K

--- pic/plotGrid.py
+++ pic/plotGrid.py
@@ -14,6 +14,7 @@ fig, ax = plt.subplots(figsize=(8,8))
 ax.matshow(Data.astype(bool), cmap='Greys',  interpolation='nearest', origin='lower')
 ax.set_xlabel("$x$")
 ax.set_ylabel("$y$")
+ax.xaxis.set_ticks_position('bottom')
 plt.savefig("Grid.png", transparent=True, dpi=300)
 plt.show()
 
@@ -28,5 +29,22 @@ fig, ax = plt.subplots(figsize=(8,8))
 ax.matshow(Data, cmap=new_map,  interpolation='nearest', origin='lower')
 ax.set_xlabel("$x$")
 ax.set_ylabel("$y$")
+ax.xaxis.set_ticks_position('bottom')
 plt.savefig("Cluster.png", transparent=True, dpi=300)
+plt.show()
+
+
+''' ---------- Zoom Plot ---------- '''
+extent = (0, L, 0, L)
+fig, ax = plt.subplots(figsize=(8,8))
+ax.matshow(Data, cmap=new_map,  interpolation='nearest', origin='lower', extent=extent)
+axins = ax.inset_axes(
+    [0.5, 0.5, 0.47, 0.47],
+    xlim=(L/3, 2*L/5), ylim=(L/3, 2*L/5), xticklabels=[], yticklabels=[])
+axins.matshow(Data, cmap=new_map,  interpolation='nearest', origin='lower', extent=extent)
+ax.indicate_inset_zoom(axins, edgecolor="black", alpha=1)
+ax.set_xlabel("$x$")
+ax.set_ylabel("$y$")
+ax.xaxis.set_ticks_position('bottom')
+plt.savefig("ZoomCluster.png", transparent=True, dpi=300)
 plt.show()
